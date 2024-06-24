84d6d30d1e902e49a1b242b2e58ff20a14441a44
Khushal <142389350+Anuragiskp@users.noreply.github.com>
2024-06-24 14:14:29 +0530

    Backend Part-I done

--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react'
 import GenderCheckbox from './GenderCheckbox'
+import { uniqueNamesGenerator, adjectives, names} from 'unique-names-generator';
 
 const Signup = () => {
+    const [username, setUsername] = useState('');
+
+    const generateUsername = () => {
+        event.preventDefault();
+        const randomUsername = uniqueNamesGenerator({
+            dictionaries: [adjectives, names], 
+            length: 2, 
+            separator: ' ', 
+            style: 'lowerCase' 
+        });
+        setUsername(randomUsername);
+    };
+
     return (
         <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
             <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-5'>
@@ -22,7 +36,8 @@ const Signup = () => {
                         <label className='label p-2'>
                             <span className='text-base label-text text-gray-400'>Username</span>
                         </label>
-                        <input type='text' placeholder='Enter username' className='w-full input input-bordered h-10' />
+                        <input type='text' placeholder='Generate username' className='w-full input input-bordered h-10' value={username} disabled/>
+                        <button className='btn btn-block btn-neutral btn-xs mt-1 bg-gray-500' onClick={generateUsername}>Generate Username</button>
                     </div>
 
                     <div>
