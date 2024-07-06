d7893b9aa58bf309f588fc86901be4481c84ca53
Khushal <142389350+Anuragiskp@users.noreply.github.com>
2024-07-06 11:17:54 +0530

    Sidebar adjusted

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,13 +1,22 @@
 import Conversation from "./Conversation"
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
 
 const Conversations = () => {
+  const {loading, conversations} = useGetConversations();
   return (
-    <div>
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-    </div>
+    <div className='py-2 flex flex-col overflow-auto'>
+			{conversations.map((conversation, idx) => (
+				<Conversation
+					key={conversation._id}
+					conversation={conversation}
+					emoji={getRandomEmoji()}
+					lastIdx={idx === conversations.length - 1}
+				/>
+			))}
+
+			{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
+		</div>
   )
 }
 
--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -14,4 +14,4 @@ const Sidebar = () => {
   )
 }
 
-export default Sidebar
+export default Sidebar;
