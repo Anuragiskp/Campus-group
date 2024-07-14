a4f0bf6c90f059f64ccef44686846fead21a44b2
Khushal <142389350+Anuragiskp@users.noreply.github.com>
2024-07-14 12:46:41 +0530

    Messaging done

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,16 +1,30 @@
-const Message = () => {
+import { useAuthContext } from '../../context/AuthContext.jsx';
+import useConversation from '../../zustand/useConversation';
+import { extractTime } from '../../utils/extractTime.js';
+
+const Message = ({message}) => {
+  const {authUser} = useAuthContext();
+  const { selectConverstion } = useConversation();
+  const fromMe = message.senderId === authUser._id;
+  const formattedTime = extractTime(message.createdAt);
+  const chatClassName = fromMe ? 'chat-end' : 'chat-start';
+  const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+
+  const shakeClass = message.shouldShake ? "shake" : "";
+
   return (
-    <div className="chat chat-end">
+    <div className={`chat ${chatClassName}`}>
         <div className="chat-image avatar">
             <div className="w-10 rounded-full">
                 <img
                     alt="Chat Bubble"
-                    src={"https://static.vecteezy.com/system/resources/previews/005/897/736/non_2x/avatar-of-a-brown-haired-man-in-a-blue-t-shirt-with-a-beard-design-element-for-forums-chat-bots-support-social-networks-illustration-vector.jpg"}
+                    src={profilePic}
                 />
             </div>
         </div>
-        <div className={`chat-bubble text-white bg-blue-500`}>Hi! What's up?</div>
-        <div className="chat-footer opacity-50 text-xs text-white flex gap-1 items-center">12:43</div>
+        <div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
+        <div className="chat-footer opacity-50 text-xs text-white flex gap-1 items-center">{formattedTime}</div>
     </div>
   )
 }
--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,16 +1,24 @@
+import useConversation from '../../zustand/useConversation.js';
 
 const Conversation = ({ conversation, lastIdx, emoji }) => {
+    const {selectedConversation, setSelectedConversation} = useConversation();
+
+    const isSelected = selectedConversation?._id === conversation._id;
+    // const isOnline = onlineUsers.includes(conversation._id);
+
     return (
         <>
-            <div className="flex gap-2 items-center hover:bg-gray-500 rounded p-2 py-1 cursor-pointer">
-                <div className='avatar online'>
-                    <div className='w-12 rounded-full'>
-                        <img
-                            src={conversation.profilePic}
-                            alt='user avatar'
-                        />
-                    </div>
-                </div>
+            <div
+				className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+				${isSelected ? "bg-sky-500" : ""}
+			`}
+				onClick={() => setSelectedConversation(conversation)}
+			>
+				<div>
+					<div className='w-12 rounded-full'>
+						<img src={conversation.profilePic} alt='user avatar' />
+					</div>
+				</div>
 
                 <div className='flex flex-col flex-1'>
                     <div className='flex gap-3 justify-between'>
