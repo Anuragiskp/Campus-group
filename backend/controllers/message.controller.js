f1893b9d522c9ede0481078a4e43a2a6386bf279
Khushal <142389350+Anuragiskp@users.noreply.github.com>
2025-01-02 14:00:17 +0530

    Project completed

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,6 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
-// import { getReceiverSocketId, io } from "../socket/socket.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
 	try {
@@ -30,11 +30,11 @@ export const sendMessage = async (req, res) => {
 
 		await Promise.all([conversation.save(), newMessage.save()]);
 
-		// const receiverSocketId = getReceiverSocketId(receiverId);
-		// if (receiverSocketId) {
-		// 	// io.to(<socket_id>).emit() used to send events to specific client
-		// 	io.to(receiverSocketId).emit("newMessage", newMessage);
-		// }
+		const receiverSocketId = getReceiverSocketId(receiverId);
+		if (receiverSocketId) {
+			// io.to(<socket_id>).emit() used to send events to specific client
+			io.to(receiverSocketId).emit("newMessage", newMessage);
+		}
 
 		res.status(201).json(newMessage);
 	} catch (error) {
--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-const app = express()
+import { app, server } from "./socket/socket.js";
+
+// const app = express()
 
 dotenv.config();
 
@@ -20,7 +22,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	connectToMongoDB();
 	console.log(`Server Running on port ${PORT}`);
 });
