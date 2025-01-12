81c9be452624f912f1d4d132d60485ff310a4ce8
Khushal <142389350+Anuragiskp@users.noreply.github.com>
2025-01-12 23:09:47 +0530

    debug

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from 'daisyui';
 export default {
   content: [
     "./index.html",
@@ -8,5 +7,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: [require("daisyui")],
 }
